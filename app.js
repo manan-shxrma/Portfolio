f89d45bfb2281e727347e0cd57a161a459a10d9b
manan-shxrma <manansharmams3@gmail.com>
2021-09-01 21:42:14 +0530

    fun retry

--- app.js
+++ app.js
@@ -2,17 +2,13 @@
 const express = require('express')
 const app = express();
 
-
-
-
-
 app.use(express.static("assets"));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname +'/index.html')
 })
 app.get('/contact', (req, res) => {
-    res.send("manan")
+    res.send("prateush lodu hai...........uski crush priya pahwa hai ")
   })
 
 app.listen(process.env.PORT || 3000, () => {
